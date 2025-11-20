397057c7e88f3df5c017dfd8de78e363c376749c
Yaseen Taha <yaseen_taha123@hotmail.com>
2025-11-20 08:55:33 -0700

    partially completed login page, new fonts, and config changes

--- src/route/Login.tsx
+++ src/route/Login.tsx
@@ -1,8 +1,25 @@
+import backround from "../assets/bg.jpg"
+import logo from "../assets/logo.png"
+import button from "../assets/button.png"
+
 function Login() {
 
   return (
-    <div className="bg-green-950 text-white">
-      <h1>Login</h1>
+    <div className="bg-repeat-y bg-center bg-contain px-8 w-[1200px] h-lvh m-auto" style={{ backgroundImage: `url(${backround})` }}>
+      <img src={logo} alt="" className="w-[60%] mx-auto" />
+
+      <div className=" text-center p-2.5 font-pixel text-[1.05rem]">
+        There are currently 67,420 players online!
+      </div>
+
+      <div className="flex justify-center gap-4 text-2xl font-semibold">
+        <button className="h-[126px] w-[152px] bg-no-repeat bg-contain hover:underline hover:cursor-pointer" style={{ backgroundImage: `url(${button})` }}>
+          Login
+        </button>
+        <button className="h-[126px] w-[152px] bg-no-repeat bg-center hover:underline hover:cursor-pointer" style={{ backgroundImage: `url(${button})` }}>
+          Register
+        </button>
+      </div>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  
+
+  server: {
+    host: true,
+    watch: {
+      usePolling: true,
+      interval: 100
+    }
+  }
+
 })
