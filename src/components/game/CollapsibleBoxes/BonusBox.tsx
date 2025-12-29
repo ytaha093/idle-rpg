28e368739387427cb0765c73433a0419a9ae7283
Yaseen Taha <yaseen_taha123@hotmail.com>
2025-12-29 06:07:56 -0700

    small ui fix that went unnoticed durring the great  font swich / restyle

--- src/components/game/CollapsibleBoxes/BonusBox.tsx
+++ src/components/game/CollapsibleBoxes/BonusBox.tsx
@@ -10,7 +10,7 @@ function BonusBox() {
         <div className="p-0.5">
             <div className=" w-full h-6 bg-linear-0 to-stone-900 from-grey4 border border-stone-800 relative">
                 <div id="meter" className="w-full h-full bg-linear-0 to-[#392f23] from-[#5c4b38]" style={{ width: `${(progress / bonusCap) * 100}%` }}></div>
-                <div className=" absolute inset-0 flex justify-center items-center font-pixel hover:cursor-default">{progress}/{bonusCap}</div>
+                <div className=" absolute inset-0 flex justify-center items-center font-pixel text-sm hover:cursor-default">{progress}/{bonusCap}</div>
             </div>
         </div>
     )
